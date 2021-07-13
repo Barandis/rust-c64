4692172df45d7a51f0ff0b74c1b3bafcca10f095
Thomas Otterson <barandis1729@gmail.com>
2021-07-12 21:07:27 -0400

    feat: 🎸 add 74258 quad 2-to-1 multiplexer

--- src/chips/mod.rs
+++ src/chips/mod.rs
@@ -8,9 +8,11 @@ mod ic7406;
 mod ic7408;
 mod ic74139;
 mod ic74257;
+mod ic74258;
 
 pub use self::ic4066::Ic4066;
 pub use self::ic7406::Ic7406;
 pub use self::ic7408::Ic7408;
 pub use self::ic74139::Ic74139;
 pub use self::ic74257::Ic74257;
+pub use self::ic74258::Ic74258;
